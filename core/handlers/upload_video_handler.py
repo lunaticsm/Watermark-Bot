9308e1505bc8b76a8e5c2f22f816a25373d9e976
Sourcery AI <>
2024-01-07 15:45:38 +0000

    'Refactored by Sourcery'

--- core/handlers/upload_video_handler.py
+++ core/handlers/upload_video_handler.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 async def send_video_handler(bot, cmd, output_vid, video_thumbnail, duration, width, height, editable, logs_msg, file_size):
     c_time = time.time()
-    sent_vid = await bot.send_video(
+    return await bot.send_video(
         chat_id=cmd.chat.id,
         video=output_vid,
         caption=f"**File Name:** `{output_vid}`\n**Video Duration:** `{format_timespan(duration)}`\n**File Size:** `{humanbytes(file_size)}`\n\n{Config.CAPTION}",
@@ -19,15 +19,25 @@ async def send_video_handler(bot, cmd, output_vid, video_thumbnail, duration, wi
         height=height,
         reply_to_message_id=cmd.message_id,
         supports_streaming=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/AbirHasan2005")],
-                                           [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")],
-                                           [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo")]]),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Developer", url="https://t.me/AbirHasan2005"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Bots Channel", url="https://t.me/Discovery_Updates"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Support Group", url="https://t.me/linux_repo"
+                    )
+                ],
+            ]
+        ),
         progress=progress_for_pyrogram,
-        progress_args=(
-            "Uploading, Wait Sir ...",
-            editable,
-            logs_msg,
-            c_time
-        )
+        progress_args=("Uploading, Wait Sir ...", editable, logs_msg, c_time),
     )
-    return sent_vid
